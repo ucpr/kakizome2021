2c54eb50c310c63e8a292f9877cb35c12e85e538
ucpr <contact@ucpr.dev>
2021-01-09 21:34:50 +0900

    style: run black

--- gaming_happy_new_year.py
+++ gaming_happy_new_year.py
@@ -44,7 +44,7 @@ template_ja = """
 ┃  ┃ ┃  ┃
 ┃  ┻━╋━ ┃
 ┃　　┃  ┃
-┗━━━━━━━┛ 
+┗━━━━━━━┛
 """
 
 
@@ -52,36 +52,36 @@ def gaming(lines, speed):
     pos = 0
     w = max([len(x) for x in lines])  # count max width
     columns = shutil.get_terminal_size().columns
-    
+
     while True:
         for i in range(16):
             for j in range(16):
                 sys.stdout.write("\x1B[2J\x1B[H")
                 sys.stdout.flush()
-    
+
                 v = i * 16 + j
                 for line in lines:
                     sys.stdout.write(f"\033[{pos}C")
                     sys.stdout.write(f"\033[38;5;{v}m{line}\033[0m\n")
-                    #print(f"\033[38;5;{v}m{k}\033[0m", end="")
+                    # print(f"\033[38;5;{v}m{k}\033[0m", end="")
                 sys.stdout.flush()
-    
+
                 pos += 1
                 if pos + w > columns:
                     pos = 0
-    
-            time.sleep(speed) 
 
-    
+            time.sleep(speed)
+
+
 def get_option():
     arg_parser = ArgumentParser()
     arg_parser.add_argument(
         "-l",
         "--language",
-	type=str,
+        type=str,
         dest="lang",
-	default="en",
-	choices=["en", "english", "ja", "japanese"],
+        default="en",
+        choices=["en", "english", "ja", "japanese"],
     )
     arg_parser.add_argument(
         "-s",
@@ -96,7 +96,7 @@ def get_option():
 def main():
     opt = get_option()
 
-    template = template_en
+    template = template_ja
     if opt.lang in ["ja", "japanese"]:
         template = template_ja
 
