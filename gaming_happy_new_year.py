0bf3ee9ffb205f48dc7e8f7974a4f6a189dde70e
ucpr <hoge.uchihara@gmail.com>
2021-01-09 17:39:53 +0900

    feat: support english

--- gaming_happy_new_year.py
+++ gaming_happy_new_year.py
@@ -1,9 +1,22 @@
+from argparse import ArgumentParser
 import time
 import sys
 import shutil
 
 
-template = """
+
+template_en = """
+##
+##
+#####   ####  #####  #####  ##  ##   #####   ####  ##   ##   ##  ##  ####   ####  #####
+##  ##     ## ##  ## ##  ## ##  ##   ##  ## ##  ## ## # ##   ##  ## ##  ##     ## ##  ##
+##  ##  ##### ##  ## ##  ## ##  ##   ##  ## ###### #######   ##  ## ######  ##### ##
+##  ## ##  ## #####  #####   #####   ##  ## ##      #####     ##### ##     ##  ## ##
+##  ##  ##### ##     ##        ##    ##  ##  #####  ## ##       ##   #####  ##### ##
+              ##     ##     ####                             ####
+"""
+
+template_ja = """
 ┏━━━━━━━┓
 ┃ ━ ╋━╋ ┃
 ┃ ━ ┏┳┓ ┃
@@ -35,26 +48,54 @@ template = """
 ┗━━━━━━━┛ 
 """
 
-lines = template.strip().split("\n")
 
-pos = 0
-columns = shutil.get_terminal_size().columns
+def gaming(lines, w=90):
+    pos = 0
+    columns = shutil.get_terminal_size().columns
+    
+    while True:
+        for i in range(16):
+            for j in range(16):
+                sys.stdout.write("\x1B[2J\x1B[H")
+                sys.stdout.flush()
+    
+                v = i * 16 + j
+                for line in lines:
+                    sys.stdout.write(f"\033[{pos}C")
+                    sys.stdout.write(f"\033[38;5;{v}m{line}\033[0m\n")
+                    #print(f"\033[38;5;{v}m{k}\033[0m", end="")
+                sys.stdout.flush()
+    
+                pos += 1
+                if pos + w > columns:
+                    pos = 0
+    
+            time.sleep(0.2) 
 
-while True:
-    for i in range(16):
-        for j in range(16):
-            sys.stdout.write("\x1B[2J\x1B[H")
-            sys.stdout.flush()
+    
+def get_option():
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "-l",
+        "--language",
+	type=str,
+        dest="lang",
+	default="en",
+	choices=["en", "english", "ja", "japanese"],
+    )
+    return arg_parser.parse_args()
 
-            v = i * 16 + j
-            for line in lines:
-                sys.stdout.write(f"\033[{pos}C")
-                sys.stdout.write(f"\033[38;5;{v}m{line}\033[0m\n")
-                #print(f"\033[38;5;{v}m{k}\033[0m", end="")
-            sys.stdout.flush()
 
-            pos += 1
-            if pos + 9 > columns:
-                pos = 0
+def main():
+    opt = get_option()
 
-        time.sleep(0.2) 
+    template = template_en
+    if opt.lang in ["ja", "japanese"]:
+        template = template_ja
+
+    lines = template.strip().split("\n")
+    gaming(lines)
+
+
+if __name__ == "__main__":
+    main()
